c229428e712819f3aea2e6c74f657e422537d2f1
nogitsune413 <nakamuray4@gmail.com>
2017-04-22 11:53:50 +0900

    改めてコミット。

---
+++ README.md
@@ -0,0 +1,93 @@
+## 概要
+Redmineのデータをバックアップする、Power Shellスクリプトです。
+
+## 機能
+1. 以下のデータをバックアップします。
+- 画像ファイルが入っている「files」フォルダ
+- その他のデータが格納されているMySQLのDB
+
+2. 外付けHDDとミラーリングし、データを2箇所で保持します。
+
+## 運用
+Windowsのタスクスケジューラを使って、定期的にバックアップを実行します。
+
+## 使用方法
+
+### 前提
+以下がインストールされていること
+- Redmine：Bitnamiのオールインワン・パッケージ
+- 7-zip
+
+### 準備
+#### MySqlへのログイン情報を設定する。
+
+redmineのデータが入っているmysqlのDBをダンプするには、DBにアクセスするためのユーザID/パスワードが必要になります。
+このアカウント情報は、以下に記述されています。
+
+C:\Bitnami\redmine_3_3_2_2\apps\redmine\htdocs\config\database.yml
+```ファイル
+production:
+  adapter: xxx
+  database: xxx
+  host: xx.xx.xx.xx
+  username: xxx
+  password: xxx
+  encoding: utf8
+  port: xxx
+```
+
+この情報を設定ファイルに追記します。
+
+config\settings.ini
+
+#### 処理対象フォルダとコマンドラインツール(EXEファイル)のパスを設定する。
+
+設定ファイルに記述します。
+
+config\setteings.ini
+
+#### タスクスケジューラへの登録
+
+このプログラムは、タスクスケジューラに登録し、定期的に実行する事を想定しています。
+タスクスケジューラでは、以下のように登録します。
+
+```
+【操作の編集】
+操作：プログラムの開始
+プログラム/スクリプト：%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe
+引数の追加(オプション)：-Command "C:\Bitnami\redmine_3_3_2_2_data_backup\scripts\redmine_3_3_2_2_backup_script.ps1"
+```
+
+### 実行
+タスクスケジューラから実行します。
+
+## フォルダ構成
+```
+【内蔵HDD】
+C:
+└─Bitnami
+    └─redmineBackup
+        ├─backup_files
+        │      redmineBackup_YYYY-MMDD-HHMMSS.7z
+        │      redmineBackup_YYYY-MMDD-HHMMSS.7z
+        │      redmineBackup_YYYY-MMDD-HHMMSS.7z
+        │
+        ├─config
+        │      settings.ini
+        │
+        ├─log
+        │      dumpMySqlDataError.log
+        │      
+        ├─scripts
+        │      redmine_3_3_2_2_backup_script.ps1
+        │          
+        └─work
+
+【外付けHDD】
+E:
+└─Bitnami
+    └─redmine_3_3_2_2_data_backup
+            redmineBackup_YYYY-MMDD-HHMMSS.7z
+            redmineBackup_YYYY-MMDD-HHMMSS.7z
+            redmineBackup_YYYY-MMDD-HHMMSS.7z
+```
---
+++ scripts/redmine_3_3_2_2_backup_script.ps1
@@ -0,0 +1,63 @@
+﻿# ---  ヘルパー関数  ---
+
+function initializeFolders ($settings) {
+    makeFolderIfNotExists (Join-Path $settings.bkRoot work)
+    makeFolderIfNotExists (Join-Path $settings.bkRoot backup_files)
+    makeFolderIfNotExists ($settings.mir)
+}
+
+function makeFolderIfNotExists($path){
+    if(-not (Test-Path $path)){
+        New-Item $path -ItemType Directory
+    }
+}
+
+# ---  初期化  ---
+
+# アクセスするリソースのパスをINIファイルから読み込みます。
+$s = Get-Content (Split-Path $MyInvocation.MyCommand.Path -Parent | Split-Path -Parent | Join-Path -ChildPath config\settings.ini) | ConvertFrom-StringData
+
+# 処理対象となるフォルダを作成します。
+initializeFolders($s)
+
+
+# ---  バックアップ  ---
+
+# 添付ファイルが格納されているfilesフォルダをworkフォルダにコピーします。
+Copy-Item -Path $s.files -Destination (Join-Path $s.bkRoot work) -Recurse
+
+# mySqlに格納されているデータをworkフォルダに取得します。
+Start-Process -NoNewWindow `
+              -FilePath $s.mysqldump `
+              -ArgumentList ("--user=" + $s.user), `
+                            ("--password=" + $s.password), `
+                            ("--host=" + $s.host), `
+                            ("--port=" + $s.port), `
+                            ("--result-file=" + (Join-Path $s.bkRoot $s.resultFile)), `
+                            ("--log-error=" + (Join-Path $s.bkRoot $s.logError)), `
+                            $s.dbname `
+              -Wait
+
+# 取得したバックアップデータを圧縮します。
+Start-Process -NoNewWindow -FilePath $s._7zip -ArgumentList a,-sdel,(Join-Path $s.bkRoot work\redmineBackup.7z),(Join-Path $s.bkRoot work\files),(Join-Path $s.bkRoot work\databaseBackUp.sql) -Wait
+
+# バックアップファイルの末尾に日付を付加します。
+$backUpFile = "redmineBackup_" + (Get-Date -Format "yyyy-MMdd-HHmmss") + ".7z"
+Rename-Item (Join-Path $s.bkRoot work\redmineBackup.7z) -NewName $backUpFile
+
+# バックアップデータを保管用フォルダに移動します。
+Move-Item -Path (Join-Path $s.bkRoot work | Join-Path -ChildPath $backUpFile)  -Destination (Join-Path $s.bkRoot backup_files)
+
+# 4世代以前のバックアップファイルを削除します。
+Get-ChildItem (Join-Path $s.bkRoot backup_files) |
+Sort-Object CreationTime -Descending |
+Select-Object -Skip 3 |
+foreach{Remove-Item -Path $_.FullName}
+
+
+# ---  ミラーリング  ---
+
+# バックアップファイルを外付けHDDとミラーリングします。
+if((Get-ChildItem (Join-Path $s.bkRoot backup_files) | Measure-Object).Count -ne 0){ # 万が一、コピー元が空で同期してしまうと、コピー先のファイルが全部消えるので。
+    ROBOCOPY (Join-Path $s.bkRoot backup_files) $s.mir /MIR
+}
